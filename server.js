51f23f57ac2bc29176c5252fcc199b04e52efed0
Sandro Nasso <sandro.ordnas@live.ca>
2018-05-08 22:13:18 -0400

    reading each file using a function and storing in a single array

--- server.js
+++ server.js
@@ -12,128 +12,68 @@ let title3 = "";
 let title4 = "";
 let title5 = "";
 
-let arr1 = [];
-let arr2 = [];
-let arr3 = [];
+let detailsArr = [];
 
 
-try {  
-    let data = fs.readFileSync(inputFile1, 'utf8');
+function fileReader(inputFile, delimiter) {
 
-    let splitData = data.split("\n");
-
-    splitData.forEach(function(line, index) {
-
-        console.log(`line: ${line}`);
-
-        let splitLine = line.split(",");
-
-        // array element at index 0 contains the row of headers
-        if (index === 0) {
-
-            title1 = splitLine[0];
-            title2 = splitLine[1];
-            title3 = splitLine[2];
-            title4 = splitLine[3];
-            title5 = splitLine[4];
-
-        } else {
-
-            let result = { 
-                [title1] : splitLine[0], 
-                [title2] : splitLine[1],
-                [title3] : splitLine[2],
-                [title4] : splitLine[3],
-                [title5] : splitLine[4].trim()
-            };
+    try {  
+        let data = fs.readFileSync(inputFile, 'utf8');
     
-            arr1.push(result);
+        let splitData = data.split("\n");
+    
+        splitData.forEach(function(line, index) {
+    
+            let splitLine = line.split(delimiter);
+    
+            if (index === 0) {
+    
+                title1 = splitLine[0].trim();
+                title2 = splitLine[1].trim();
+                title3 = splitLine[2].trim();
+                title4 = splitLine[3].trim();
+                title5 = splitLine[4].trim();
+    
+            } else {
 
-            // console.log(JSON.stringify(result));
-        }
-    });
+                let origin = splitLine[0].trim();
+                let departureTime = splitLine[1].replace(/-/gi, '/').trim();
+                let destination = splitLine[2].trim();
+                let destinationTime = splitLine[3].replace(/-/gi, '/').trim();
+                let price = splitLine[4].trim();
 
-    console.log(`arr1:`);
-    console.log(arr1);    
-} catch(e) {
-    console.log('Error:', e.stack);
+                let result = { 
+                    [title1] : origin,
+                    [title2] : departureTime,
+                    [title3] : destination,
+                    [title4] : destinationTime,
+                    [title5] : price
+                };
+
+                console.log(`${origin} --> ${destination} (${departureTime} --> ${destinationTime}) - ${price}`);
+        
+                detailsArr.push(result);
+            }
+        });
+    } catch(e) {
+        console.log('Error:', e.stack);
+    }
 }
 
 
-try {  
-    let data = fs.readFileSync(inputFile2, 'utf8');
+console.log(`Provider1.txt:`);
+fileReader(inputFile1, ",");
 
-    let splitData = data.split("\n");
+console.log(`Provider2.txt:`);
+fileReader(inputFile2, ",");
 
-    splitData.forEach(function(line, index) {
-
-        let splitLine = line.split(",");
-
-        if (index === 0) {
-
-            title1 = splitLine[0];
-            title2 = splitLine[1];
-            title3 = splitLine[2];
-            title4 = splitLine[3];
-            title5 = splitLine[4];
-
-        } else {
-
-            let result = { 
-                [title1] : splitLine[0], 
-                [title2] : splitLine[1],
-                [title3] : splitLine[2],
-                [title4] : splitLine[3],
-                [title5] : splitLine[4].trim()
-            };
-    
-            arr2.push(result);
-        }
-    });
-
-    console.log(`arr2:`);
-    console.log(arr2);    
-} catch(e) {
-    console.log('Error:', e.stack);
-}
+console.log(`Provider3.txt:`);
+fileReader(inputFile3, "|");
 
 
-try {  
-    let data = fs.readFileSync(inputFile3, 'utf8');
-
-    let splitData = data.split("\n");
-
-    splitData.forEach(function(line, index) {
-
-        let splitLine = line.split("|");
-
-        if (index === 0) {
-
-            title1 = splitLine[0];
-            title2 = splitLine[1];
-            title3 = splitLine[2];
-            title4 = splitLine[3];
-            title5 = splitLine[4];
-
-        } else {
-
-            let result = { 
-                [title1] : splitLine[0], 
-                [title2] : splitLine[1],
-                [title3] : splitLine[2],
-                [title4] : splitLine[3],
-                [title5] : splitLine[4].trim()
-            };
-    
-            arr3.push(result);
-        }
-    });
-
-    console.log(`arr3:`);
-    console.log(arr3);    
-} catch(e) {
-    console.log('Error:', e.stack);
-}
+console.log("Details:");
+console.log(detailsArr);
+console.log(`detailsArr.length: ${detailsArr.length}`);
 
 
 // var args = process.argv.slice(2);
