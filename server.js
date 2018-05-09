5e5160bfb0b16247a98e460a43680ae80937df89
Sandro Nasso <sandro.ordnas@live.ca>
2018-05-08 21:14:09 -0400

    uninstalled csv-parse and used fs instead making file reading synchronous

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const readlineSync  = require('readline-sync');
 const fs            = require('fs');
-const parse         = require('csv-parse');
 
  
 let inputFile1 = 'Provider1.txt';
@@ -16,28 +15,36 @@ let title5 = "";
 let arr1 = [];
 let arr2 = [];
 let arr3 = [];
- 
-let parser1 = parse({delimiter: ','}, function (err, data) {
 
-    data.forEach(function(line, index) {
+
+try {  
+    let data = fs.readFileSync(inputFile1, 'utf8');
+
+    let splitData = data.split("\n");
+
+    splitData.forEach(function(line, index) {
+
+        console.log(`line: ${line}`);
+
+        let splitLine = line.split(",");
 
         // array element at index 0 contains the row of headers
         if (index === 0) {
 
-            title1 = line[0];
-            title2 = line[1];
-            title3 = line[2];
-            title4 = line[3];
-            title5 = line[4];
+            title1 = splitLine[0];
+            title2 = splitLine[1];
+            title3 = splitLine[2];
+            title4 = splitLine[3];
+            title5 = splitLine[4];
 
         } else {
 
             let result = { 
-                [title1] : line[0], 
-                [title2] : line[1],
-                [title3] : line[2],
-                [title4] : line[3],
-                [title5] : line[4]
+                [title1] : splitLine[0], 
+                [title2] : splitLine[1],
+                [title3] : splitLine[2],
+                [title4] : splitLine[3],
+                [title5] : splitLine[4].trim()
             };
     
             arr1.push(result);
@@ -45,75 +52,88 @@ let parser1 = parse({delimiter: ','}, function (err, data) {
             // console.log(JSON.stringify(result));
         }
     });
-    
-    console.log("\nProvider1.txt:");
-    console.log(JSON.stringify(arr1));
-});
 
-let parser2 = parse({delimiter: ','}, function (err, data) {
+    console.log(`arr1:`);
+    console.log(arr1);    
+} catch(e) {
+    console.log('Error:', e.stack);
+}
 
-    data.forEach(function(line, index) {
+
+try {  
+    let data = fs.readFileSync(inputFile2, 'utf8');
+
+    let splitData = data.split("\n");
+
+    splitData.forEach(function(line, index) {
+
+        let splitLine = line.split(",");
 
         if (index === 0) {
 
-            title1 = line[0];
-            title2 = line[1];
-            title3 = line[2];
-            title4 = line[3];
-            title5 = line[4];
+            title1 = splitLine[0];
+            title2 = splitLine[1];
+            title3 = splitLine[2];
+            title4 = splitLine[3];
+            title5 = splitLine[4];
 
         } else {
 
             let result = { 
-                [title1] : line[0], 
-                [title2] : line[1],
-                [title3] : line[2],
-                [title4] : line[3],
-                [title5] : line[4]
+                [title1] : splitLine[0], 
+                [title2] : splitLine[1],
+                [title3] : splitLine[2],
+                [title4] : splitLine[3],
+                [title5] : splitLine[4].trim()
             };
     
             arr2.push(result);
         }
     });
-    
-    console.log("\nProvider2.txt:");
-    console.log(JSON.stringify(arr2));
-});
 
-let parser3 = parse({delimiter: '|'}, function (err, data) {
+    console.log(`arr2:`);
+    console.log(arr2);    
+} catch(e) {
+    console.log('Error:', e.stack);
+}
 
-    data.forEach(function(line, index) {
+
+try {  
+    let data = fs.readFileSync(inputFile3, 'utf8');
+
+    let splitData = data.split("\n");
+
+    splitData.forEach(function(line, index) {
+
+        let splitLine = line.split("|");
 
         if (index === 0) {
 
-            title1 = line[0];
-            title2 = line[1];
-            title3 = line[2];
-            title4 = line[3];
-            title5 = line[4];
+            title1 = splitLine[0];
+            title2 = splitLine[1];
+            title3 = splitLine[2];
+            title4 = splitLine[3];
+            title5 = splitLine[4];
 
         } else {
 
             let result = { 
-                [title1] : line[0], 
-                [title2] : line[1],
-                [title3] : line[2],
-                [title4] : line[3],
-                [title5] : line[4]
+                [title1] : splitLine[0], 
+                [title2] : splitLine[1],
+                [title3] : splitLine[2],
+                [title4] : splitLine[3],
+                [title5] : splitLine[4].trim()
             };
     
             arr3.push(result);
         }
     });
-    
-    console.log("\nProvider3.txt:");
-    console.log(JSON.stringify(arr3));
-});
 
-
-fs.createReadStream(inputFile1).pipe(parser1);
-fs.createReadStream(inputFile2).pipe(parser2);
-fs.createReadStream(inputFile3).pipe(parser3);
+    console.log(`arr3:`);
+    console.log(arr3);    
+} catch(e) {
+    console.log('Error:', e.stack);
+}
 
 
 // var args = process.argv.slice(2);
